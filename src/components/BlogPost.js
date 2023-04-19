70d28d6e7abf79268f2ac3292444d87372027eb1
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-19 23:43:17 +0300

    first commit

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,11 +1,11 @@
 import React from "react";
-import BlogContent from "./BlogContent";
+import BloggContent from "./BlogContent";
 import Comment from "./Comment";
 
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BloggContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
